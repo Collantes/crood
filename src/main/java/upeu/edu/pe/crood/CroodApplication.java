43d8c0e4342def393cecc73aa6f0aa002987fe98
Collantes <systemcollantes@gmail.com>
2022-07-01 08:34:18 -0500

    Mi primer commit

---
+++ src/main/java/upeu/edu/pe/crood/CroodApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.crood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/crood/CroodApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.crood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
